24c7664c3f58ac86e6d44054f76522bcfae56e3b
daikicci <anyayanova@yandex.ru>
2022-06-01 22:14:02 +0300

    [fix] minor

--- task_3.py
+++ task_3.py
@@ -2,7 +2,7 @@
 # Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например, пользователь ввёл число 3.
 # Считаем 3 + 33 + 333 = 369.
 
-user_number = input('Введите число: ')
+user_number = input('Введите число n: ')
 try:
     temp_user_number = int(user_number)
 except ValueError:
