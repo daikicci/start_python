7b5c0017bb25715b9cfe61762cb6c2e98bcb4587
daikicci <anyayanova@yandex.ru>
2022-06-01 22:01:47 +0300

    [feat] task 3

---
+++ task_3.py
@@ -0,0 +1,9 @@
+# Задание 3.
+# Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например, пользователь ввёл число 3.
+# Считаем 3 + 33 + 333 = 369.
+
+user_number = input('Введите число: ')
+double_number = user_number + user_number
+triple_number = double_number + user_number
+final_number = int(user_number) + int(double_number) + int(triple_number)
+print(f'Сумма чисел n + nn + nnn: {final_number}')
