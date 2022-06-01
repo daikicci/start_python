fdf799850ac3b37389f66b6ecadd35f6e1b5a916
daikicci <anyayanova@yandex.ru>
2022-06-01 21:50:21 +0300

    [fix] quotes

--- task_1.py
+++ task_1.py
@@ -11,16 +11,16 @@ sister_name = 'Нацу'
 sister_age = 6
 print('А мою сестру зовут {1} {0}, ей {2} лет.'.format(sister_name, my_last_name, sister_age))
 
-user_name = input("Введи своё имя: ")
-apple_number = int(input("Сколько яблок ты сегодня съел? "))
-banana_number = int(input("А бананов? "))
+user_name = input('Введи своё имя: ')
+apple_number = int(input('Сколько яблок ты сегодня съел? '))
+banana_number = int(input('А бананов? '))
 orange_number = None
-ate_orange = input("Ел ли ты апельсины? ")
+ate_orange = input('Ел ли ты апельсины? ')
 if ate_orange.lower() != 'да' and ate_orange.lower() != 'нет':
     print(f'Мам, а {user_name} не хочет со мной разговаривать!')
 else:
     if ate_orange.lower() == 'да':
-        orange_number = int(input("А сколько? "))
+        orange_number = int(input('А сколько? '))
     print(f'{user_name} сегодня кушал много фруктов, вот они слева направо:\n'
           f'Яблоки: {apple_number:<10}'
           f'Бананы: {banana_number:<10}'
