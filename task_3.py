40a4652e4d1de23e96ae8c8bdcaabbff71a9df95
daikicci <anyayanova@yandex.ru>
2022-06-01 22:08:59 +0300

    [feat] validation

--- task_3.py
+++ task_3.py
@@ -3,7 +3,12 @@
 # Считаем 3 + 33 + 333 = 369.
 
 user_number = input('Введите число: ')
-double_number = user_number + user_number
-triple_number = double_number + user_number
-final_number = int(user_number) + int(double_number) + int(triple_number)
-print(f'Сумма чисел n + nn + nnn: {final_number}')
+try:
+    temp_user_number = int(user_number)
+except ValueError:
+    print('Эй, это не число!')
+else:
+    double_number = user_number + user_number
+    triple_number = double_number + user_number
+    final_number = int(user_number) + int(double_number) + int(triple_number)
+    print(f'Сумма чисел n + nn + nnn: {final_number}')
