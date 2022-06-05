6b17e0aa0188a64d4b754800963335af88ead84d
daikicci <anyayanova@yandex.ru>
2022-06-05 11:57:10 +0300

    [feat] task_1

---
+++ task_1.py
@@ -0,0 +1,23 @@
+# Задание 1. Создать список и заполнить его элементами различных типов данных. Реализовать скрипт проверки типа данных
+# каждого элемента. Использовать функцию type() для проверки типа. Элементы списка можно не запрашивать у пользователя,
+# а указать явно, в программе.
+
+int = 394
+float = 9.75
+complex = complex(5, 4)
+str = 'крокодил'
+list = ['ехал', 'грека', 'через', 'реку']
+tuple = (4, 8, 15, 16, 23, 42)
+set = set('император')
+frozenset = frozenset('императрица')
+dict = {'name': 'Шоё', 'last_name': 'Хината', 'age': 15}
+bool = True
+bytes = b'word'
+bytearray = bytearray(bytes)
+none = None
+
+my_list = [int, float, complex, str, list, tuple, set, frozenset, dict, bool, bytes, bytearray, none]
+
+print(f'Типы элементов списка:')
+for i in range(len(my_list)):
+    print(f'{i+1}. {my_list[i]} - {type(my_list[i])}')
